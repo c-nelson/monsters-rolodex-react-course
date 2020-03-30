ccef613cc1edc3feeb7b4ced6a26534ccc5c25f5
Christian <nelson.christian.m@gmail.com>
2020-03-30 19:56:54 -0400

    Updates

---
+++ precache-manifest.5b63f6dd141eac091fb8c65eaa1ddb34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9edfc4f6f8a1782d55c86ef889e41c0e",
+    "url": "/monsters-rolodex-react-course/index.html"
+  },
+  {
+    "revision": "9758fe537f0ed56208bb",
+    "url": "/monsters-rolodex-react-course/static/css/main.5a991a3f.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex-react-course/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-react-course/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9758fe537f0ed56208bb",
+    "url": "/monsters-rolodex-react-course/static/js/main.61bb2a2e.chunk.js"
+  },
+  {
+    "revision": "dd8c4fc0ffe06b521dd9",
+    "url": "/monsters-rolodex-react-course/static/js/runtime-main.9be01896.js"
+  }
+]);
